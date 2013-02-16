d0c74f698889507cab44867f82239bf720f45aa7
Slava Bezgachev <bezgachev@gmail.com>
2013-02-16 18:23:27 +0000

    First commit. Blank project in Eclipse created.
    Created a special icon

---
+++ gen/uk/ac/reading/se2ja11/SlavaBezgachev/deepspaceodyssey/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package uk.ac.reading.se2ja11.SlavaBezgachev.deepspaceodyssey;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
